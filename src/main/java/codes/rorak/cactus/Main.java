bd5416a081b21c5038c6af772e11188084db19c6
RORAK <rorakcodes@gmail.com>
2022-05-24 15:44:45 +0200

    Added start for bot

--- src/main/java/codes/rorak/cactus/Main.java
+++ src/main/java/codes/rorak/cactus/Main.java
@@ -1,4 +1,11 @@
 package codes.rorak.cactus;
 
+import codes.rorak.cactus.bot.Bot;
+
 public class Main {
+    public static void main(String[] args) {
+        Logger.init();
+        WebServer.start();
+        Bot.start();
+    }
 }
